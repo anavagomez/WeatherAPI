2e48a72ffc42b76f1245ada1dab200f9eefe90a1
Alejandro Nava-Gómez <itortu@gmail.com>
2018-11-08 14:28:18 -0600

    Initial commit

---
+++ WeatherAPI/Default.aspx
@@ -0,0 +1,94 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WeatherAPI.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Display Daily Weather Forecast</title>
+    <style type="text/css">
+        body {
+            font-family: Arial;
+            font-size: 10pt;
+        }
+
+        table {
+            border: 1px solid #ccc;
+            border-collapse: collapse;
+        }
+
+            table th {
+                background-color: #f7f7f7;
+                color: #333;
+                font-weight: bold;
+            }
+
+            table th, table td {
+                padding: 5px;
+                border: 1px solid #ccc;
+            }
+
+            table, table table td {
+                border: 0px solid #ccc;
+            }
+
+        .badge {
+            display: inline-block;
+            min-width: 10px;
+            padding: 3px 7px;
+            font-size: 12px;
+            font-weight: 700;
+            color: #fff;
+            line-height: 1;
+            vertical-align: baseline;
+            white-space: nowrap;
+            text-align: center;
+            background-color: #999;
+            border-radius: 10px;
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:TextBox ID="txtCity" runat="server" Text="" />
+        <asp:Button Text="Get Weather Information" runat="server" OnClick="GetWeatherInfo" />
+        <hr />
+        <table id="tblWeather" runat="server" border="0" cellpadding="0" cellspacing="0" visible="false">
+            <tr>
+                <th colspan="2">Weather Information</th>
+            </tr>
+            <tr>
+                <td rowspan="4">
+                    <asp:Image ID="imgWeatherIcon" runat="server" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblCity_Country" runat="server" />
+                    <asp:Image ID="imgCountryFlag" runat="server" />
+                    <asp:Label ID="lblDescription" runat="server" />                    
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span class="badge badge-info">
+                        <asp:Label ID="lblTemperature" runat="server"/> 
+                    </span>
+                    temperature from
+                    <asp:Label ID="lblTempMin" runat="server" />
+                    to
+                    <asp:Label ID="lblTempMax" runat="server" />,
+                    humidity  
+                    <asp:Label ID="lblHumidity" runat="server" />%,
+                    wind                     
+                    <asp:Label ID="lblWind" runat="server" /> m/s.
+                    clouds 
+                    <asp:Label ID="lblClouds" runat="server" />%,
+                    <asp:Label ID="lblPressure" runat="server" /> hpa
+                </td>
+            </tr>
+            <tr>
+                <td>Geo coords [<asp:Label ID="lblLat" runat="server" />, <asp:Label ID="lblLon" runat="server" />]                    
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ WeatherAPI/Default.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Xml;
+using System.Web.Script.Serialization;
+
+namespace WeatherAPI
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void GetWeatherInfo(object sender, EventArgs e)
+        {
+            string appId = "07b188606c1f2c32a63d78a88a6e9652";
+
+            string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}&units=metric&cnt=1", txtCity.Text.Trim(), appId);
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+
+                WeatherResponse weatherResponse = (new JavaScriptSerializer()).Deserialize<WeatherResponse>(json);
+
+                lblCity_Country.Text = weatherResponse.name + ", " + weatherResponse.sys.country;
+                imgCountryFlag.ImageUrl = string.Format("http://openweathermap.org/images/flags/{0}.png", weatherResponse.sys.country.ToLower());
+                lblDescription.Text = weatherResponse.weather[0].description;
+                imgWeatherIcon.ImageUrl = string.Format("http://openweathermap.org/img/w/{0}.png", weatherResponse.weather[0].icon);
+                lblTemperature.Text = string.Format("{0}°С", weatherResponse.main.temp);
+                lblHumidity.Text = weatherResponse.main.humidity.ToString();
+                lblWind.Text = weatherResponse.wind.speed.ToString();
+                lblClouds.Text = weatherResponse.clouds.all.ToString();
+                lblTempMin.Text = weatherResponse.main.temp_min.ToString();
+                lblTempMax.Text = string.Format("{0}°С", weatherResponse.main.temp_max);
+                lblPressure.Text = weatherResponse.main.pressure.ToString();
+                lblLon.Text = weatherResponse.coord.lon.ToString();
+                lblLat.Text = weatherResponse.coord.lat.ToString();
+                tblWeather.Visible = true;
+            }
+        }
+
+        public class Coord
+        {
+            public double lon { get; set; }
+            public double lat { get; set; }
+        }
+
+        public class Weather
+        {
+            public int id { get; set; }
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+
+        public class Main
+        {
+            public double temp { get; set; }
+            public int pressure { get; set; }
+            public int humidity { get; set; }
+            public int temp_min { get; set; }
+            public int temp_max { get; set; }
+
+        }
+        public class Wind
+        {
+            public double speed { get; set; }
+            public double deg { get; set; }
+        }
+
+        public class Clouds
+        {
+            public int all { get; set; }
+        }
+
+        public class Sys
+        {
+            public int type { get; set; }
+            public int id { get; set; }
+            public double message { get; set; }
+            public string country { get; set; }
+            public int sunrise { get; set; }
+            public int sunset { get; set; }
+        }
+
+        public class WeatherResponse
+        {
+            public Coord coord { get; set; }
+            public List<Weather> weather { get; set; }
+            public string @base { get; set; }
+            public Main main { get; set; }
+            public int visibility { get; set; }
+            public Wind wind { get; set; }
+            public Clouds clouds { get; set; }
+            public int dt { get; set; }
+            public Sys sys { get; set; }
+            public int id { get; set; }
+            public string name { get; set; }
+            public int cod { get; set; }
+
+        }
+
+    }
+}
